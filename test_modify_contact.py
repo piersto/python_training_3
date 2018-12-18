da5f097a02f8ec388815810804680be56ace32af
PStoiko <pstoiko@gmail.com>
2018-12-18 14:31:22 -0500

    Оптимизация ввода данных в формы | Added test_modify_contact

---
+++ test_modify_contact.py
@@ -0,0 +1,8 @@
+from model.contact import Contact
+
+
+def test_modify_contact_firstname(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.modify_first_contact(Contact(firstname='New first name'))
+    app.session.logout()
+
