1eb1cae24e810759e3f55b5d4aa655f71d9fe62d
PStoiko <pstoiko@gmail.com>
2018-12-18 16:04:12 -0500

    Оптимизация ввода данных в формы |  test_modify_contact passed

--- fixture/contact.py
+++ fixture/contact.py
@@ -13,17 +13,24 @@ class ContactHelper:
 
     def fill_in_contact_form(self, contact):
         wd = self.app.wd
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.middlename)
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        self.fill_contact_form(contact)
         Select(wd.find_element_by_name("bday")).select_by_visible_text("1")
         wd.find_element_by_name("bday").click()
         self.submit_contact_form()
         self.app.return_on_home_page()
+
+    def fill_contact_form(self, contact):
+        wd = self.app.wd
+        self.change_field_value('firstname', contact.firstname)
+        self.change_field_value('middlename', contact.middlename)
+        self.change_field_value('lastname', contact.lastname)
+
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
 
     def open_add_new_contact_page(self):
         wd = self.app.wd
@@ -31,6 +38,23 @@ class ContactHelper:
 
     def delete_first_contact(self):
         wd = self.app.wd
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_contact()
         wd.find_element_by_xpath("//input[@value='Delete']").click()
         wd.switch_to.alert.accept()
+
+    def select_first_contact(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+
+    def modify_first_contact(self, new_contact_data):
+        wd = self.app.wd
+        self.select_first_contact()
+        # click modification button
+        wd.find_element_by_css_selector("img[alt=\"Edit\"]").click()
+        # specify new data
+        self.fill_contact_form(new_contact_data)
+        # click update button
+        wd.find_element_by_name("update").click()
+        # return to home page
+        self.app.return_on_home_page()
+
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -6,3 +6,8 @@ def test_modify_contact_firstname(app):
     app.contact.modify_first_contact(Contact(firstname='New first name'))
     app.session.logout()
 
+
+def test_modify_contact_middlename(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.modify_first_contact(Contact(middlename='II'))
+    app.session.logout()
