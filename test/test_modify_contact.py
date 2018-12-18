13ee490dc98c3f5e36b0bb2e914047e2cd6191d4
PStoiko <pstoiko@gmail.com>
2018-12-18 16:14:46 -0500

    Повышаем "интеллектуальность" фикстуры | removed login and logouts from tests

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,12 +2,8 @@ from model.contact import Contact
 
 
 def test_modify_contact_firstname(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(firstname='New first name'))
-    app.session.logout()
 
 
 def test_modify_contact_middlename(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(middlename='II'))
-    app.session.logout()
