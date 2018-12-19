0f68c3b049504198c4456860faaff5f173ec7de1
Pierre <pstoiko@gmail.com>
2018-12-18 22:24:55 -0500

    Задание №9: Обеспечить выполнение предусловий в тестах для удаления и модификации для групп и контактов |corrected mistake in test_del_contact

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -3,7 +3,7 @@ from model.contact import Contact
 
 
 def test_delete_first_contact(app):
-    if app.contact.count == 0:
+    if app.contact.count() == 0:
         app.contact.open_add_new_contact_page()
         app.contact.fill_contact_form(Contact('Contact to be deleted'))
     app.contact.delete_first_contact()
