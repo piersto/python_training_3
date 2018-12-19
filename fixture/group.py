01ed7c984dd0c701dfd4b6218f277218cf539582
Pierre <pstoiko@gmail.com>
2018-12-18 20:50:19 -0500

    Повышаем "интеллектуальность" тестов: обеспечение выполнения предусловий | Done

--- fixture/group.py
+++ fixture/group.py
@@ -59,5 +59,13 @@ class GroupHelper:
         self.return_to_groups_page()
 
 
+    def count(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        return len(wd.find_elements_by_name("selected[]"))
+
+
+
+
 
 
